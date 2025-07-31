79fc95d952a4730561704da1f8496848fea07b35
Rodrigo Miguel Santos Rodrigues <98055826+rmsr2004@users.noreply.github.com>
2025-07-31 18:41:33 +0100

    chore/pipelines definition (#2)
    * feat: pipelines definiton for prod and dev
    
    	- This should be tested in feat/ branches
    
    * chore: adds examples to do testing
    
    * fix: adds config to import vars from .env file
    
    * fix: remove integration test to test the pipeline
    
    * fix: docker-compose to docker compose

---
+++ src/test/java/com/myauth/DomainTests/Test1Tests.java
@@ -0,0 +1,13 @@
+package com.myauth.DomainTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class Test1Tests {
+
+    @Test
+    public void testSum() {
+        int result = 2 + 3;
+        assertEquals(5, result);
+    }
+}
