3c3a860dc7ae45f63f7251889f66fb1f55a25fc6
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2025-07-31 18:05:48 +0100

    chore: adds examples to do testing

---
+++ src/test/java/com/myauth/IntegrationTests/Test1Tests.java
@@ -0,0 +1,15 @@
+package com.myauth.IntegrationTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class Test1Tests {
+
+    @Test
+    public void contextLoads() {
+        assertTrue(true);
+    }
+}
