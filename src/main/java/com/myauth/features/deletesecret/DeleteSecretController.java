16937f46e9dd4dc4eb660c942fe30766c8eb47ed
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2026-01-23 13:47:45 +0000

    chore: adds documentation

--- src/main/java/com/myauth/features/deletesecret/DeleteSecretController.java
+++ src/main/java/com/myauth/features/deletesecret/DeleteSecretController.java
@@ -12,8 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.myauth.common.utils.ErrorDto;
 import com.myauth.common.utils.Result;
+import com.myauth.features.addsecret.AddSecretResponseDto;
 import com.myauth.infrastructure.db.entities.User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 
@@ -23,6 +29,25 @@ import lombok.AllArgsConstructor;
 public class DeleteSecretController {
     private final DeleteSecretHandler handler;
 
+    @Operation(summary="Deletes a secret for a specific user")
+    @ApiResponses(value={
+        @ApiResponse(responseCode="204", description="User successfully deleted a secret", content=@Content(
+            mediaType="application/json",
+            schema=@Schema(implementation=AddSecretResponseDto.class)
+        )),
+        @ApiResponse(responseCode="400", description="Bad Request", content=@Content(
+            mediaType="application/json",
+            schema=@Schema(implementation = ErrorDto.class)
+        )),
+        @ApiResponse(responseCode="403", description="Forbidden", content=@Content(
+            mediaType="application/json",
+            schema=@Schema(implementation = ErrorDto.class)
+        )),
+        @ApiResponse(responseCode="500", description="Internal Server Error", content=@Content(
+            mediaType="application/json",
+            schema=@Schema(implementation = ErrorDto.class)
+        ))
+    })
     @DeleteMapping("/{secretId}")
     public ResponseEntity<?> deleteSecret(@AuthenticationPrincipal User user, HttpServletRequest request, @PathVariable Long secretId) {
         Result<Void> result = handler.deleteSecretForUser(secretId, user.getId());
