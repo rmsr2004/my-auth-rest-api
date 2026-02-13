41f9f56463334c69406d17c9050ae4e58470698e
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2026-02-13 22:09:00 +0000

    refactor: updates folder struct to the final version

--- src/main/java/com/myauth/conf/spring/startup/PostRunPrint.java
+++ src/main/java/com/myauth/conf/spring/startup/PostRunPrint.java
@@ -1,4 +1,4 @@
-package com.myauth.conf.spring;
+package com.myauth.conf.spring.startup;
 
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Profile;
