fa9484f9dd31633b0b1a87054b43047cc86ba5e4
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2026-02-16 19:47:30 +0000

    feat: add cors config

--- src/main/java/com/myauth/conf/spring/security/SecurityConfig.java
+++ src/main/java/com/myauth/conf/spring/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.myauth.conf.spring.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -25,6 +26,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .csrf(csrf -> csrf.disable())
+            .cors(Customizer.withDefaults())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.PUT, "/api/auth/login").permitAll()
