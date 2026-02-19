2de261b8721cd69f785798f07873a8ccc7e77f6b
rmsr2004 <rodrigomiguelsr2004@gmail.com>
2026-02-19 10:35:14 +0000

    fix: removes @SpringBootTest from infra unit tests

--- src/test/java/com/myauth/DomainTests/Infrastructure/Security/PasswordEncoderTests.java
+++ src/test/java/com/myauth/DomainTests/Infrastructure/Security/PasswordEncoderTests.java
@@ -4,12 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles("test")
 @DisplayName("Password Encoder Tests")
 class PasswordEncoderTests {
     @Autowired
--- src/test/java/com/myauth/DomainTests/Infrastructure/Security/TokenServiceTests.java
+++ src/test/java/com/myauth/DomainTests/Infrastructure/Security/TokenServiceTests.java
@@ -4,15 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 import com.myauth.infrastructure.db.entities.User;
 import com.myauth.infrastructure.db.repositories.IUserRepository;
 import com.myauth.infrastructure.security.TokenService;
 
-@SpringBootTest
-@ActiveProfiles("test")
 @DisplayName("Token Service Tests")
 class TokenServiceTests {
     @Autowired
